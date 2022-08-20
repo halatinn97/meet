d24c2e51ab92943ef259ce1c727f88febae90bf3
Hala Tinn <hala79tinn@gmail.com>
2022-08-20 22:00:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+import Event from './Event';
+
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <Event />
+      <NumberOfEvents />
     </div>
   );
 }
