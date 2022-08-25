71dc1fac6eed828ebc939ef427194b1c346470cb
Hala Tinn <hala79tinn@gmail.com>
2022-08-25 18:52:23 +0200

    Change existing ft.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
     state = {
         query: '',
-        suggestions: []
+        suggestions: [],
+        showSuggestions: undefined
     }
 
     handleInputChanged = (event) => {
@@ -19,7 +20,8 @@ class CitySearch extends Component {
 
     handleItemClicked = (suggestion) => {
         this.setState({
-            query: suggestion
+            query: suggestion,
+            showSuggestions: false
         });
         this.props.updateEvents(suggestion);
     }
@@ -27,14 +29,14 @@ class CitySearch extends Component {
     render() {
         return (
             <div className="CitySearch">
-
                 <input
                     type="text"
                     className="city"
                     value={this.state.query}
                     onChange={this.handleInputChanged}
+                    onFocus={() => { this.setState({ showSuggestions: true }) }}
                 />
-                <ul className="suggestions">
+                <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
                     {this.state.suggestions.map((suggestion) => (
                         <li key={suggestion}
                             onClick={() => this.handleItemClicked(suggestion)}
