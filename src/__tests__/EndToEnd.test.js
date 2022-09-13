5a37f4a61f24eddac5e5cb3133a972c4f99c3ed5
Hala Tinn <hala79tinn@gmail.com>
2022-09-13 02:37:19 +0200

    Bug fix: Update naming after Event.js update

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -21,19 +21,19 @@ describe('show/hide an event details', () => {
     });
 
     test('An event element is collapsed by default', async () => {
-        const eventDetails = await page.$('.event .event-details');
+        const eventDetails = await page.$('.event .description');
         expect(eventDetails).toBeNull();
     });
 
     test('User can expand an event to see its details', async () => {
-        await page.click('.event .details-btn');
-        const eventDetails = await page.$('.event .event-details');
+        await page.click('.event .show-details-btn');
+        const eventDetails = await page.$('.event .description');
         expect(eventDetails).toBeDefined();
     });
 
     test('User can collapse an event to hide its details', async () => {
-        await page.click('.event .event-details');
-        const eventDetails = await page.$('.event .event-details');
+        await page.click('.event .hide-details-btn');
+        const eventDetails = await page.$('.event .description');
         expect(eventDetails).toBeNull();
     });
 });
