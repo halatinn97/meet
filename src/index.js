a003e611d4d930fe2a69791108569ebca0c72c2e
Hala Tinn <hala79tinn@gmail.com>
2022-09-15 11:46:11 +0200

    Update manifest & set up serviceWorker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 
+atatus.config('6937680201344489b6f7b4d507cb8614').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,8 +15,6 @@ root.render(
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 reportWebVitals();
-
-atatus.config('6937680201344489b6f7b4d507cb8614').install();
