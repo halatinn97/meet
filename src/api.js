8012879426e1f345c504f0b9f8304f9b6b1ed5ef
Hala Tinn <hala79tinn@gmail.com>
2022-09-20 21:56:20 +0200

    Refactor api.js

--- src/api.js
+++ src/api.js
@@ -36,7 +36,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url = 'https://lq19kh08g7.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const url = `https://lq19kh08g7.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
@@ -52,7 +52,7 @@ const getToken = async (code) => {
     try {
         const encodeCode = encodeURIComponent(code);
 
-        const response = await fetch('https://lq19kh08g7.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
+        const response = await fetch(`https://lq19kh08g7.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
         }
