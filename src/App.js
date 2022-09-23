2e2fa770bed4c80c6ceb4cef7dc66476873940cc
Hala Tinn <hala79tinn@gmail.com>
2022-09-24 00:39:48 +0200

    Prefix & process CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './style.production.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
